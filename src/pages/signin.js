94b063716f02e4f474d7c32d5d0ccd458c846c2e
KRATIKA MITTAL <56781757+kratika19@users.noreply.github.com>
2021-05-31 19:31:56 +0530

    styling sign in

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import SignIn from '../components/SignIn'
+import ScrollToTop from '../components/scrollToTop'
 const SigninPage = () => {
     return (
         <>
+            <ScrollToTop />
             <SignIn />
         </>
     )
